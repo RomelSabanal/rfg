5543f50eef233563586321b37075d49858d4fd87
Romel Sabanal <141883574+RomelSabanal@users.noreply.github.com>
2024-10-12 10:10:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add smooth scroll for navigation
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
